0fdb750ce27378705a6285e6a6c432245729f6a1
Diego Navarro <code@diegognt.dev>
2023-01-21 11:37:21 -0500

    Code analysis tools.
    This PR includes:
      - ESlint set up
      - Prettier set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Yet another web client consuming the Poke API</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
