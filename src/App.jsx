181c9aabcb937910106d20b5e0f31931726010de
Diego Navarro <code@diegognt.dev>
2023-01-28 10:45:16 -0500

    Search and Result components layout
    This PR includes:
      - Initial layout for a "Search" component
      - Initial layout for a "Results" component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Results from "./Results";
+import Search from "./Search";
+
 function App() {
   return (
-    <div className="flex justify-center items-center w-full min-h-screen">
-      <h1 className="text-6xl">Yet another web Pokedex using the Poke API</h1>
-    </div>
+    <>
+      <Search />
+      <Results />
+    </>
   );
 }
 
