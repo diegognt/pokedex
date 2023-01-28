da607c9b496e3a5f2d8105643e51aeb9a7316a78
Diego Navarro <code@diegognt.dev>
2023-01-28 10:45:16 -0500

    Search and Result components layout
    This PR includes:
      - Initial layout for a "Search" component
      - Initial layout for a "Results" component

---
+++ src/Results.jsx
@@ -0,0 +1,9 @@
+const Results = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-5/6 p-4">
+      <h1 className="text-6xl">Yet another web Pokedex using the Poke API</h1>
+    </div>
+  )
+}
+
+export default Results
---
+++ src/Search.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const Search = () => {
+  const [pokemonName, setPokemonName] = useState('')
+
+  return (
+    <div className="flex flex-row flex-wrap items-center justify-center w-full p-4 h-1/6">
+      <form className="basis-4/6">
+        <div className="w-full">
+          <label
+            className="block"
+            htmlFor="pokemon-name">
+            <input
+              className="block border-2 border-cyan-800 text-cyan-800 text-2xl rounded p-8 w-full h-20"
+              id="pokemon-name"
+              type="text"
+              name="pokemon-name"
+              placeholder="Type a Pokemon name or ID to search for it"
+              value={pokemonName}
+              onChange={(event) => setPokemonName(event.target.value)} />
+          </label>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
